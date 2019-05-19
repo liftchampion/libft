dd0fd2f15c28f3c330c1c9d00e73143010f93cf8
ggerardy <liftchampion@gmail.com>
2019-05-19 21:35:01 +0300

    testing submodules

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ggerardy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 17:25:22 by ggerardy          #+#    #+#             */
-/*   Updated: 2019/05/19 21:24:03 by ggerardy         ###   ########.fr       */
+/*   Updated: 2019/05/19 21:34:06 by ggerardy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ int				ft_atoi(const char *str)
 	long long int	res;
 	long long int	res_tmp;
 
+
+
+
+
+
 	res = 0;
 	while (*str && ft_isspace(*str))
 		str++;
@@ -26,6 +31,10 @@ int				ft_atoi(const char *str)
 		str++;
 	while (*str && ft_isdigit(*str))
 	{
+
+
+
+
 		res_tmp = res;
 		res = res * 10 + (*str++ - '0') * sign;
 		if ((res < res_tmp && sign == 1) || (res > res_tmp && sign == -1))
